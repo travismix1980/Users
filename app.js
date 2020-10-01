27147ce574effd8fecbaa185142e9ee287a1a717
Travis Mix <travismix1980@gmail.com>
2020-10-01 15:24:58 -0700

    Basic Nodejs server created

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
